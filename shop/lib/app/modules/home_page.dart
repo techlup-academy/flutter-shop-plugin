1977c5c2bd50195716ffd8dcf731273fc40ea772
denisdevke <deniskem.dev@gmail.com>
2023-07-26 17:30:10 +0300

    added product item widget

--- shop/lib/app/modules/home_page.dart
+++ shop/lib/app/modules/home_page.dart
@@ -3,6 +3,7 @@ import 'package:shop/app/modules/home_controller.dart';
 import 'package:get/get.dart';
 import 'package:shop/app/config.dart';
 import 'package:shop/app/widgets/product_card_view.dart';
+import 'package:shop/app/widgets/product_item.dart';
 import 'package:shop/app/models/product.dart';
 
 class HomePage extends StatelessWidget {
@@ -11,11 +12,28 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+
+
     return GetBuilder<HomeController>(
       builder: (controller) => Scaffold(
-        backgroundColor: backgroundColor,
-        body: Center(
-          child: ProductCardView(product:  Product())
+        backgroundColor: Colors.black12,
+        body: LayoutBuilder(
+        builder: (context, constraints){
+          double screenWidth = constraints.maxWidth;
+          double desiredItemWidth = 200;
+          int crossAxisCount = (screenWidth / desiredItemWidth).floor();
+          return  GridView.count(
+            crossAxisCount: crossAxisCount,
+            padding: const EdgeInsets.all(1.0),
+            childAspectRatio: 8.0/9.0,
+            children: [
+              ProductItem(name: 'Onions', imageUrl: 'https://picsum.photos/id/292/1200/800', slug: 'laptop', price: '40.00',),
+              ProductItem(name: 'Common Beans', imageUrl: 'https://picsum.photos/id/766/1200/800', slug: 'laptop', price: '500.00',),
+              ProductItem(name: 'French Beans', imageUrl: 'https://picsum.photos/id/627/1200/800', slug: 'laptop', price: '350.00',),
+            ],);
+
+        }
         ),
       ),
     );
--- shop/lib/app/widgets/product_item.dart
+++ shop/lib/app/widgets/product_item.dart
@@ -1,41 +1,112 @@
 import 'package:flutter/material.dart';
 
-class ProductItems extends StatelessWidget {
-  const ProductItems({super.key});
+class ProductItem extends StatelessWidget {
+
+  String name;
+  String imageUrl;
+  String slug;
+  String price;
+
+  ProductItem(
+      {super.key,
+        required this.name,
+        required this.imageUrl,
+        required this.slug,
+        required this.price
+      });
 
   @override
   Widget build(BuildContext context) {
-      return const Card(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            ListTile(
-              leading: Icon(Icons.album),
-              title: Text('The Enchanted Nightingale'),
-              subtitle: Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
+    return InkWell(
+      onTap: () {
+
+      },
+      child: Container(
+        padding: const EdgeInsets.only(top: 5),
+        child: Card(
+            color: Colors.white,
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(20.0),
+              ),
             ),
-            Row(
-              children: <Widget>[
-                CircularProgressIndicator(),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[
-                TextButton(
-                  onPressed: null,
-                  child: Text('BUY TICKETS'),
+            elevation: 0,
+            child: Column(
+              children: <Widget>[Padding(
+                padding: const EdgeInsets.all(10.0),
+                child:
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(10.0), // Set the desired border radius
+                  child:
+                Image.network(
+                imageUrl,
+              ),),
+              ),
+                Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.only(left: 10, right: 10, top: 5),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text("Ksh.",
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontFamily: 'Roboto-Light.ttf',
+                            fontSize: 15,
+                            fontWeight: FontWeight.w400)),
+                      Expanded(child: Text((price != null) ? price : 'Unavailable',
+                          textAlign: TextAlign.center,
+                          style: const TextStyle(
+                              color: Colors.black,
+                              fontFamily: 'Roboto-Light.ttf',
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold)),),
+                      const Icon(Icons.star, size: 15, color: Colors.amber,),
+                      const Text("4.4",
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                              color: Colors.black45,
+                              fontFamily: 'Roboto-Light.ttf',
+                              fontSize: 15,
+                              fontWeight: FontWeight.w400)),
+                    ],
+                  ),
                 ),
-                SizedBox(width: 8),
-                TextButton(
-                  onPressed: null,
-                  child: Text('LISTEN'),
+                Container(
+                  padding: const EdgeInsets.only(left: 10, right: 10, top: 5),
+                  alignment: Alignment.centerLeft,
+                  child: Text( (name.length <= 40 ? name : name.substring(0, 40)),
+                      style: TextStyle(
+                          color: (price != null)
+                              ? const Color(0xFFf67426)
+                              : const Color(0xFF0dc2cd),
+                          fontFamily: 'Roboto-Light.ttf',
+                          fontSize: 12,
+                          fontWeight: FontWeight.w500)),
                 ),
-                SizedBox(width: 8),
+                Container(
+                  alignment: Alignment.bottomLeft,
+                  padding: const EdgeInsets.only(left: 10, right: 10, top: 5),
+                  child: const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('27 July 2023',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontFamily: 'Roboto-Light.ttf',
+                              fontSize: 10,
+                              fontWeight: FontWeight.w500)),
+                      Expanded(child: Text('')
+                      ),
+                      Icon(Icons.add_shopping_cart, size: 25, color: Colors.blue,),
+                    ],
+                  ),
+                )
               ],
-            ),
-          ],
-        ),
-      );
+            )),
+      ),
+    );
   }
 }
